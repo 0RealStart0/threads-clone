7829a766bc070c88c3486a65f1041064a849756b
0realstart0 <0realstart0@gmail.com>
2024-06-12 18:35:09 +0900

    api key 수정

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB0pfOnn5qSHXT2CrKi2Bj0CVgy-5J67sI",
-  authDomain: "threads-clone-4b217.firebaseapp.com",
-  projectId: "threads-clone-4b217",
-  storageBucket: "threads-clone-4b217.appspot.com",
-  messagingSenderId: "693402318290",
-  appId: "1:693402318290:web:34cacb2dfc76f0742f840b",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET_KEY,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
